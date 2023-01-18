39ad2e6fd7f469b24b07c44991292800a9f43545
grem <mes510@yandex.ru>
2023-01-18 20:50:07 +0300

    created EvenNumberGame.js

---
+++ EvenNumberGame.js
@@ -0,0 +1,40 @@
+import inquirer from "inquirer";
+
+const isEven = (number) => {
+    if (number % 2 === 0) return true;
+    else return false;
+};
+
+const expectedAnswer = (result) => {
+    if (result === true) return 'yes';
+    else return 'no';
+}
+
+const evenNumberGame = async() => {
+    let rightAnswers = 0;
+    let wrongAnswers = 0;
+
+    while (rightAnswers < 3 && wrongAnswers < 1) {
+        const randomNumber = Math.floor(Math.random() * (100 - 0));
+        const result = isEven(randomNumber);
+        const answer = expectedAnswer(result);
+
+        await inquirer
+        .prompt([
+            {
+                name: 'userAnswer',
+                message:`Is the number ${randomNumber} even? yes/no?`
+            }
+        ])
+        .then(answers => {
+            if (answers.userAnswer === answer) {
+                console.info('The answer is right!');
+                rightAnswers += 1;
+            } else {
+                console.info('The answer is wrong!');
+                wrongAnswers += 1;
+            };
+        });
+    }
+};
+evenNumberGame();
