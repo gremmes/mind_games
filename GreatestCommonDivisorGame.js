dc498f17fc3724cb6cd174ea8f0f008d56251473
grem <mes510@yandex.ru>
2023-01-27 17:13:42 +0300

    created GreatestCommonDivisorGame.js

---
+++ GreatestCommonDivisorGame.js
@@ -0,0 +1,56 @@
+import inquirer from "inquirer";
+
+const findDivisors = (number) => {
+    const result = [ 1 ];
+
+    for (let i = 2; i <= number; i += 1) {
+        if (number % i === 0) result.push(i);
+    }
+    
+    return result;
+};
+
+const getArrayIntersection = (arr1, arr2) => {
+    const result = [];
+
+    for (let i = 0; i < arr1.length; i += 1) {
+        if (arr2.includes(arr1[i])) result.push(arr1[i]);
+    }
+    
+    return result;
+};
+
+const greatestCommonDivisorGame = async() => {
+    let rightAnswers = 0;
+    let wrongAnswers = 0;
+
+    while (rightAnswers < 3 && wrongAnswers < 1) {
+        // the randomMultiplier is not necessary but it gives more options
+        const randomMultiplier = Math.floor(Math.random() * 10) + 1;
+        const firstNumber = (Math.floor(Math.random() * 10) + 1) * randomMultiplier;
+        const secondNumber = (Math.floor(Math.random() * 10) + 1) * randomMultiplier;
+        const firstNumberDivisors = findDivisors(firstNumber);
+        const secondNumberDivisors = findDivisors(secondNumber);
+        const arrayIntersection = getArrayIntersection(firstNumberDivisors, secondNumberDivisors);
+        const greatestCommonDivisor = arrayIntersection.pop();
+
+        await inquirer
+        .prompt([
+            {
+                name: 'userAnswer',
+                message: `What is the greatest common divisor of the numbers ${firstNumber} and ${secondNumber}?`,
+            }
+        ])
+        .then(answers => {
+            if (answers.userAnswer === greatestCommonDivisor.toString()) {
+                console.info('The answer is right!');
+                rightAnswers += 1;
+            } else {
+                console.info('The answer is wrong!');
+                wrongAnswers += 1;
+            }
+        })
+    }
+};
+
+greatestCommonDivisorGame();
